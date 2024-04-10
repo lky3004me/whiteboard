c67d993bf34bcecde7942396a9ef6afc798a4ae8
hyunjo325 <hyunjo325@naver.com>
2024-04-10 21:57:44 +0900

    double buffering

--- src/main/java/DrawInfo.java
+++ src/main/java/DrawInfo.java
@@ -16,7 +16,7 @@ import java.util.Vector;
 class DrawInfo {
     private int x; // 처음 클릭 좌표
     private int y;
-    private int x1; //나중 클릭 좌표
+    private int x1; //나중 클릭 좌표s
     private int y1;
     private String type; //도형 모양 PEN, LINE, CIR, REC
     private Color color; //색깔 RGB
@@ -25,6 +25,7 @@ class DrawInfo {
     private int color_B;
     private boolean fill;// 색 채우기 여부
     private int thickness; //선두께
+
     public DrawInfo(String type, int x, int y, int x1, int y1,
                     int color_R, int color_G, int color_B,
                     boolean fill, int thickness) {
@@ -55,8 +56,14 @@ class DrawInfo {
         public CMServerStub m_serverStub = null;
         public boolean isClient = false; //클라이언트: true 서버: false
         private int x; private int y; private int x1; private int y1;
-
+        public Graphics Graphics_buffer; //더블버퍼링
+        public Image Img_buffer;
+        public JFrame jFrame;
         public JButton exitBtn = new JButton("나가기");
+        public JButton penBtn = new JButton("펜");
+        public JButton lineBtn = new JButton("직선");
+        public JButton cirBtn = new JButton("원");
+        public JButton recBtn = new JButton("사각형");
         public JPanel btnPanel = new JPanel();
 
         public DrawFrame(CMClientStub m_clientStub){
@@ -68,11 +75,24 @@ class DrawInfo {
             this.addMouseMotionListener(this);
             this.setSize(500,500);
 
-
             //나가기 버튼
             //버튼에 이벤트 핸들러 등록, 상단에 버튼 추가
             exitBtn.addActionListener(this);
             btnPanel.add(exitBtn);
+
+            penBtn.addActionListener(this);
+            btnPanel.add(penBtn);
+
+            lineBtn.addActionListener(this);
+            btnPanel.add(lineBtn);
+
+            cirBtn.addActionListener(this);
+            btnPanel.add(cirBtn);
+
+            recBtn.addActionListener(this);
+            btnPanel.add(recBtn);
+
+
             this.add(btnPanel, BorderLayout.NORTH);
 
             setLocationRelativeTo(null);
@@ -96,18 +116,28 @@ class DrawInfo {
             setLocationRelativeTo(null);
             this.setVisible(true);
         }//
+        //@Override
         public void paint(Graphics g){
-
+            //더블 버퍼링을 위한 설정
+            Img_buffer = createImage(getWidth(),getHeight());
+            Graphics_buffer = Img_buffer.getGraphics();
+            update(g);
+        }
+        //@Override
+        public void update(Graphics g){
             //벡터에 저장된 그림 전부 그림
+            Graphics_buffer.clearRect(0, 0, 500, 500); // 백지화
             for (int i = 0; i < vc.size(); i++) {
                 DrawInfo info = (DrawInfo) vc.elementAt(i);
-                g.setColor(info.color);
-                ((Graphics2D) g).setStroke(new BasicStroke(info.thickness, BasicStroke.CAP_ROUND, 0));
-                g.drawLine(info.getX(), info.getY(), info.getX1(), info.getY1());
+                Graphics_buffer.setColor(info.color);
+                ((Graphics2D) Graphics_buffer).setStroke(new BasicStroke(info.thickness, BasicStroke.CAP_ROUND, 0));
+                Graphics_buffer.drawLine(info.getX(), info.getY(), info.getX1(), info.getY1());
             }
-            firstDrawing = false;
+            g.drawImage(Img_buffer,0,0,this);
+            repaint();
 
         }
+
 
         public Vector getVc(){
             return vc;
@@ -161,6 +191,7 @@ class DrawInfo {
                 DrawInfo di = new DrawInfo("PEN", x, y, x1, y1, 0, 0, 0, false, 3);
                 vc.add(di);
                 this.repaint();
+
             }
         }
 
